33caafe3bf25e01e3653c39f22a2f2b8809c090f
시나버스 <tutor@shinahverse.kr>
2023-10-04 12:12:06 +0900

    canvas에 집 그리기
    canvas에 집 그리기

--- js/paintBoard.js
+++ js/paintBoard.js
@@ -4,4 +4,28 @@
 * 최초작성자: 곽신아
 * 이력: 
 */
-console.log(">>>paintBoard.js");
+//
+const canvas = document.getElementById("canvas");
+//
+const ctx = canvas.getContext("2d");
+
+canvas.width = 400;
+canvas.height = 500;
+
+//line 설정
+ctx.lineWidth = 5;
+
+// 벽
+ctx.strokeRect(75, 140 , 150, 110);
+
+//문
+ctx.fillRect(130, 190, 40, 60);
+
+//지붕
+ctx.beginPath();
+ctx.moveTo(50,140);
+ctx.lineTo(150,60);
+ctx.lineTo(250, 140);
+ctx.closePath();
+ctx.fillStyle = "green";
+ctx.fill();
