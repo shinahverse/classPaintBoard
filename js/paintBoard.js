57858243736b267cccf5e1656325d863c7b2d434
시나버스 <tutor@shinahverse.kr>
2023-10-11 10:12:50 +0900

    사람 그리기

--- js/paintBoard.js
+++ js/paintBoard.js
@@ -3,30 +3,40 @@
 * 최초작성일: 2023.10.04
 * 최초작성자: 곽신아
 * 이력: 2023.10.10 집 그리기
+*       2023.10.11 사람 그리기
 */
-//
+
 const canvas = document.getElementById("canvas");
-//
 const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
-ctx.rect(50,50,100,100);
-ctx.stroke();
 
-//두번째 사각형 green
-ctx.beginPath();
-ctx.rect(150,150,100,100);
-ctx.fillStyle="green";
+//얼굴
+ctx.arc(370, 150, 50, 0, 2*Math.PI);
 ctx.fill();
 
+//눈
 ctx.beginPath();
-ctx.rect(250,250,100,100);
+ctx.arc(340,180,8, Math.PI, 2*Math.PI);
+ctx.arc(380,180,8, Math.PI, 2*Math.PI);
+ctx.fillStyle="white";
+ctx.fill();
+
+//몸
+ctx.beginPath();
 ctx.fillStyle="red";
-ctx.fill();
+ctx.fillRect(350, 220, 60, 200);
 
+//팔
 ctx.beginPath();
-ctx.rect(350,350,100,100);
-// ctx.fillStyle="red";
-ctx.strokeStyle="red";
-ctx.stroke();
+ctx.fillStyle="black";
+ctx.fillRect(310, 220, 15, 100);
+ctx.fillRect(430, 220, 15, 100);
+
+//발
+ctx.beginPath();
+ctx.arc(360,430,18, 2*Math.PI, 3*Math.PI);
+ctx.arc(400,430,18, 2*Math.PI, 3*Math.PI);
+ctx.fillStyle="black";
+ctx.fill();
